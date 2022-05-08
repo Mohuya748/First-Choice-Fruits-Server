e9cf995d90c7b1f3eea24aa6d724547001517723
Mohuya748 <khandakermohuya@gmail.com>
2022-05-08 10:07:35 +0600

    updated

--- index.js
+++ index.js
@@ -1,112 +1,118 @@
 const express = require('express');
-const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const res = require('express/lib/response');
-require('dotenv').config()
-const port =process.env.PORT || 5000;
+require('dotenv').config();
 const app = express();
+const port = process.env.PORT || 5000;
 
-// middleware 
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
-function verifyJWT(req,res,next){
+function verifyJWT(req, res, next) {
   const authHeader = req.header.authorization;
-  if(!authHeader){
-    return res.status(401).send({message:'unauthorized access'});
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unauthorized access' });
   }
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-    if(err){
-      return res.status(403).send({message:'forbidden access'});
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' });
 
     }
-    console.log('decoded',decoded);
-    req.decoded  = decoded;
-     next();
+    console.log('decoded', decoded);
+    req.decoded = decoded;
+    next();
   })
- 
- 
+
+
 }
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vannh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        await client.connect();
-        const inventoryCollection = client.db('FirstChoiceFruits').collection('inventory');
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client.db('FirstChoiceFruits').collection('inventory');
 
-        // AUTH
-        app.post('/login',async(req,res) => {
-          const user =req.body;
-          const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET , { expiresIn: '2d'});
-          res.send({accessToken});
-        })
+    // AUTH
+    app.post('/login', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2d' });
+      res.send({ accessToken });
+    })
 
-        // GET a user
-        app.get('/inventory',async(req,res)=>{
-         const query = {};
-         const cursor= inventoryCollection.find(query);
-         const inventories = await cursor.toArray();
-         res.send(inventories);
-         });
+    // GET a user
+    app.get('/inventory', async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
 
-         
-         app.get('/inventory/:id',async(req,res)=>{
-          const id = req.params.id;
-          const query = {_id: ObjectId(id)};
-          const result = await inventoryCollection.findOne(query);
-          res.send(result);
 
-         });
+    app.get('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryCollection.findOne(query);
+      res.send(result);
 
-         app.put('/inventory/:id',async(req,res)=>{
-          const id = req.params.inventoryId;
-          const updateUser = req.body;
-          const filter = {_id: ObjectId(id)};
-          const options ={upsert: true};
-          const updateDoc ={
-            $set:{
-             Quantity: updateUser.Quantity
-              
-            }
-          };
-          const result = await inventoryCollection.updateOne(filter,updateDoc,options);
-          res.send(result);
-         })
+    });
 
-         // POST: add a new user
-        app.post('/inventory',async(req,res)=>{
-          const newInventory = req.body;
-          console.log("adding new  item", newInventory);
-          const result = await inventoryCollection.insertOne(newInventory);
-          res.send({result})
-         })
-        
-
-         app.get('/myitems/:email',async(req,res)=>{
-           const result = await inventoryCollection.find({email:req.params.email,}).toArray();
-           res.send(result);
-         })
-
-          //  Delete a user 
-        app.delete('/inventory/:id',async(req,res)=>{
-          const id = req.params.id;
-          const query = {_id: ObjectId(id)};
-          const result = await inventoryCollection.deleteOne(query);
-          res.send(result);
-        })
-
-        
-
+    // update item quantity 
+    app.put('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateItem = req.body;
+      console.log(id, updateItem)
+      const filter = { _id: ObjectId(id) };
+      console.log(filter)
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          Quantity: updateItem.quantity,
         }
-        finally{
-    
-        }
-    }
-    run().catch(console.dir);
+      };
+      console.log(updatedDoc)
+      const result = await inventoryCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+
+    })
+
+
+
+    // POST: add a new user
+    app.post('/inventory', async (req, res) => {
+      const newInventory = req.body;
+      console.log("adding new  item", newInventory);
+      const result = await inventoryCollection.insertOne(newInventory);
+      res.send({ result })
+    })
+
+
+    app.get('/myitems/:email', async (req, res) => {
+      const result = await inventoryCollection.find({ email: req.params.email, }).toArray();
+      res.send(result);
+    })
+
+    //  Delete a user 
+    app.delete('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
+  }
+  finally {
+
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
@@ -114,5 +120,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log('CRUD server running',port);
+  console.log('CRUD server running', port);
 })
